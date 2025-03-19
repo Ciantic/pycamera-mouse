7afc152dc7f6311f276d95a6517840630e347762
Jari Pennanen <ciantic@oksidi.com>
2025-03-19 23:22:55 +0200

    Document and fix hci0 class

--- bluetooth/server.py
+++ bluetooth/server.py
@@ -15,7 +15,7 @@ from logging import error
 logging.basicConfig(level=logging.DEBUG)
 
 
-class BtPiKeyboardMouseDevice():
+class BtPiKeyboardMouseDevice:
     # change these constants
     MY_ADDRESS = "B8:27:EB:C5:B3:27"
     MY_DEV_NAME = "Raspberry Pi Keyboard and Mouse"
@@ -48,17 +48,30 @@ class BtPiKeyboardMouseDevice():
         print("4. Configuring Bluez Profile")
         # setup profile options
         service_record = self.read_sdp_service_record()
-        opts = {
-            "AutoConnect": True,
-            "ServiceRecord": service_record
-        }
+        opts = {"AutoConnect": True, "ServiceRecord": service_record}
         # retrieve a proxy for the bluez profile interface
         bus = dbus.SystemBus()
-        manager = dbus.Interface(bus.get_object(
-            "org.bluez", "/org/bluez"), "org.bluez.ProfileManager1")
+        manager = dbus.Interface(
+            bus.get_object("org.bluez", "/org/bluez"), "org.bluez.ProfileManager1"
+        )
         manager.RegisterProfile("/org/bluez/hci0", BtPiKeyboardMouseDevice.UUID, opts)
         print("6. Profile registered ")
-        os.system("hciconfig hci0 class 0x0025C0")
+
+        # https://www.bluetooth.com/wp-content/uploads/Files/Specification/Assigned_Numbers.html#bookmark17
+        #
+        # Major service class: bits 13-23
+        # Major device class:  bits 8-12
+        # Minor device class:  bits 2-7
+        # 
+        # Mouse + Keyboard:
+        # Major service class: zeros
+        # Major device class: 10th bit 1, 8th bit 1 = Peripheral (mouse, joystick, keyboard, ...)
+        # Minor device class: 7th bit 1, 6th bit 1 = Combo Keyboard/Pointing Device
+        #
+        #        43219 9876 5432 1098 7654 3210
+        #       0b0000_0000_0000_0101_1100_0000 = 0x5C0
+
+        os.system("hciconfig hci0 class 0x0005C0")
 
     # read and return an sdp record from a file
     def read_sdp_service_record(self):
@@ -73,9 +86,11 @@ class BtPiKeyboardMouseDevice():
     def listen(self):
         print("\033[0;33m7. Waiting for connections\033[0m")
         self.scontrol = socket.socket(
-            socket.AF_BLUETOOTH, socket.SOCK_SEQPACKET, socket.BTPROTO_L2CAP)  
+            socket.AF_BLUETOOTH, socket.SOCK_SEQPACKET, socket.BTPROTO_L2CAP
+        )
         self.sinterrupt = socket.socket(
-            socket.AF_BLUETOOTH, socket.SOCK_SEQPACKET, socket.BTPROTO_L2CAP)  
+            socket.AF_BLUETOOTH, socket.SOCK_SEQPACKET, socket.BTPROTO_L2CAP
+        )
         self.scontrol.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sinterrupt.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
@@ -88,12 +103,16 @@ class BtPiKeyboardMouseDevice():
         self.sinterrupt.listen(5)
 
         self.ccontrol, cinfo = self.scontrol.accept()
-        print (
-            "\033[0;32mGot a connection on the control channel from %s \033[0m" % cinfo[0])
+        print(
+            "\033[0;32mGot a connection on the control channel from %s \033[0m"
+            % cinfo[0]
+        )
 
         self.cinterrupt, cinfo = self.sinterrupt.accept()
-        print (
-            "\033[0;32mGot a connection on the interrupt channel from %s \033[0m" % cinfo[0])
+        print(
+            "\033[0;32mGot a connection on the interrupt channel from %s \033[0m"
+            % cinfo[0]
+        )
 
     # send a string to the bluetooth host machine
     def send_string(self, message):
@@ -108,37 +127,35 @@ class BtPiKeyboardMouseService(dbus.service.Object):
     def __init__(self):
         print("1. Setting up service")
         # set up as a dbus service
-        bus_name = dbus.service.BusName(
-            "org.example.pikmservice", bus=dbus.SystemBus())
-        dbus.service.Object.__init__(
-            self, bus_name, "/org/example/pikmservice")
+        bus_name = dbus.service.BusName("org.example.pikmservice", bus=dbus.SystemBus())
+        dbus.service.Object.__init__(self, bus_name, "/org/example/pikmservice")
         # create and setup our device
         self.device = BtPiKeyboardMouseDevice()
         # start listening for connections
         self.device.listen()
 
-    @dbus.service.method('org.example.pikmservice', in_signature='yay')
+    @dbus.service.method("org.example.pikmservice", in_signature="yay")
     def send_keys(self, modifier_byte, keys):
         # print("Get send_keys request through dbus")
         # print("key msg: ", keys)
-        state = [ 0xA1, 1, 0, 0, 0, 0, 0, 0, 0, 0 ]
+        state = [0xA1, 1, 0, 0, 0, 0, 0, 0, 0, 0]
         state[2] = int(modifier_byte)
         count = 4
         for key_code in keys:
-            if(count < 10):
+            if count < 10:
                 state[count] = int(key_code)
             count += 1
         # print("keyboard state: ", state)
         self.device.send_string(state)
 
-    @dbus.service.method('org.example.pikmservice', in_signature='yay')
+    @dbus.service.method("org.example.pikmservice", in_signature="yay")
     def send_mouse(self, modifier_byte, keys):
         # print("Get send_mouse request through dbus")
         # print("mouse msg: ", keys)
         state = [0xA1, 2, 0, 0, 0, 0]
         count = 2
         for key_code in keys:
-            if(count < 6):
+            if count < 6:
                 state[count] = int(key_code)
             count += 1
         # print("mouse state: ", state)
